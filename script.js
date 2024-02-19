0adf5aadfea9c08c68dd8fca5a956eac0466fd7f
ArthurRSaldanha <arthurrsaldanha@gmail.com>
2024-02-19 17:10:14 -0300

    codificador de texto

---
+++ script.js
@@ -0,0 +1,43 @@
+let sound = new Audio('sound/glitch-sound-effect-12796.mp3');
+
+let button = document.querySelector('button');
+let textarea  = document.getElementById('textInput');
+let decoder = document.getElementById('decoder');
+let mensagem = document.getElementById('message');
+
+button.addEventListener('click', function(){
+    textarea.style.animation = ""
+    setTimeout(() => textarea.style.animation = "buttonGlitch .5s linear", 5)
+    decoder.style.animation = ""
+    setTimeout(() => decoder.style.animation = "buttonGlitch .5s linear, 5", 5) 
+    sound.play()
+    sound.volume = 0.1
+    sound.playbackRate = 1.5
+});
+
+
+function codificar(){
+    let text = textarea.value;
+
+    let criptoText = text.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+
+    mensagem.innerHTML = criptoText;
+}
+
+function decodificar(){
+    let text = textarea.value;
+
+    var decriptoText = text.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+
+    document.getElementById('message').innerHTML = decriptoText; 
+}
+
+function copiar(){
+    let copyText = document.getElementById('message').innerHTML;
+    console.log(copyText);
+
+    navigator.clipboard.writeText(copyText);
+}
+
+
+
